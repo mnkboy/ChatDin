ff5e0dcd361f28a7a800557a7b4451006f7c0018
Angel de la Cruz Casanova Rivas <Angel@MacBook-Pro-de-Macbook.local>
2020-08-04 22:56:47 -0500

    se limpio el archivo consulta.go para que unicamente la funcion de consultar a la base de datos sql server

--- consulta.go
+++ consulta.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 	"log"
 
@@ -36,80 +35,17 @@ func main() {
 	}
 	fmt.Printf("Connected!\n")
 
-	// Create employee
-	createID, err := CreateUser("ccvvf", "usuario2", "12345", true, "./imagenes/fotoPerfil2.jpeg")
+	//Read usuarios
+	count, err := ReadUsuarios()
 	if err != nil {
-		log.Fatal("Error creating Employee: ", err.Error())
-	}
-	fmt.Printf("Inserted ID: %s successfully.\n", createID)
-
-	// Read employees
-	/*count, err := ReadEmployees()
-	if err != nil {
-		log.Fatal("Error reading Employees: ", err.Error())
+		log.Fatal("Error reading Usuarios: ", err.Error())
 	}
 	fmt.Printf("Read %d row(s) successfully.\n", count)
 
-	// Update from database
-	updatedRows, err := UpdateEmployee("Jake", "Poland")
-	if err != nil {
-		log.Fatal("Error updating Employee: ", err.Error())
-	}
-	fmt.Printf("Updated %d row(s) successfully.\n", updatedRows)
-
-	// Delete from database
-	deletedRows, err := DeleteEmployee("Jake")
-	if err != nil {
-		log.Fatal("Error deleting Employee: ", err.Error())
-	}
-	fmt.Printf("Deleted %d row(s) successfully.\n", deletedRows)
-	*/
 }
 
-// CreateUser inserts an usuarios record
-func CreateUser(idUsuario string, nickname string, password string, estado bool, imagenPerfil string) (string, error) {
-	ctx := context.Background()
-	var err error
-
-	if db == nil {
-		err = errors.New("CreateUser: db is null")
-		return "error", err
-	}
-
-	// Check if database is alive.
-	err = db.PingContext(ctx)
-	if err != nil {
-		return "error", err
-	}
-
-	tsql := "INSERT INTO usuarios (idUsuario, nickname, password, estado, imagenPerfil) VALUES (@idUsuario, @nickname, @password , @estado , @imagenPerfil);"
-
-	stmt, err := db.Prepare(tsql)
-	if err != nil {
-		return "error", err
-	}
-	defer stmt.Close()
-
-	row := stmt.QueryRowContext(
-		ctx,
-		sql.Named("idUsuario", idUsuario),
-		sql.Named("nickname", nickname),
-		sql.Named("password", password),
-		sql.Named("estado", estado),
-		sql.Named("imagenPerfil", imagenPerfil))
-
-	var newidUsuario string
-
-	err = row.Scan(&newidUsuario)
-	if err != nil {
-		return "error", err
-	}
-
-	return newidUsuario, nil
-}
-
-// ReadEmployees reads all employee records
-func ReadEmployees() (int, error) {
+// ReadUsuarios reads all employee records
+func ReadUsuarios() (int, error) {
 	ctx := context.Background()
 
 	// Check if database is alive.
@@ -118,7 +54,7 @@ func ReadEmployees() (int, error) {
 		return -1, err
 	}
 
-	tsql := fmt.Sprintf("SELECT Id, Name, Location FROM TestSchema.Employees;")
+	tsql := fmt.Sprintf("SELECT idUsuario, nickname, password, estado, imagenPerfil FROM usuarios;")
 
 	// Execute query
 	rows, err := db.QueryContext(ctx, tsql)
@@ -132,64 +68,18 @@ func ReadEmployees() (int, error) {
 
 	// Iterate through the result set.
 	for rows.Next() {
-		var name, location string
-		var id int
+		var idUsuario, nickname, password, imagenPerfil string
+		var estado bool
 
 		// Get values from row.
-		err := rows.Scan(&id, &name, &location)
+		err := rows.Scan(&idUsuario, &nickname, &password, &estado, &imagenPerfil)
 		if err != nil {
 			return -1, err
 		}
 
-		fmt.Printf("ID: %d, Name: %s, Location: %s\n", id, name, location)
+		fmt.Printf("ID: %s, nickname: %s, Password: %s, Estado: %t, UbicacionFoto: %s \n", idUsuario, nickname, password, estado, imagenPerfil)
 		count++
 	}
 
 	return count, nil
-}
-
-// UpdateEmployee updates an employee's information
-func UpdateEmployee(name string, location string) (int64, error) {
-	ctx := context.Background()
-
-	// Check if database is alive.
-	err := db.PingContext(ctx)
-	if err != nil {
-		return -1, err
-	}
-
-	tsql := fmt.Sprintf("UPDATE TestSchema.Employees SET Location = @Location WHERE Name = @Name")
-
-	// Execute non-query with named parameters
-	result, err := db.ExecContext(
-		ctx,
-		tsql,
-		sql.Named("Location", location),
-		sql.Named("Name", name))
-	if err != nil {
-		return -1, err
-	}
-
-	return result.RowsAffected()
-}
-
-// DeleteEmployee deletes an employee from the database
-func DeleteEmployee(name string) (int64, error) {
-	ctx := context.Background()
-
-	// Check if database is alive.
-	err := db.PingContext(ctx)
-	if err != nil {
-		return -1, err
-	}
-
-	tsql := fmt.Sprintf("DELETE FROM TestSchema.Employees WHERE Name = @Name;")
-
-	// Execute non-query with named parameters
-	result, err := db.ExecContext(ctx, tsql, sql.Named("Name", name))
-	if err != nil {
-		return -1, err
-	}
-
-	return result.RowsAffected()
 }
